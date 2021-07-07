901bcf9cbb091dd0be16bbe29ee568b7ae2f06af
Harman1997-96 <hs802686@gmail.com>
2021-07-07 19:02:06 +0530

    create-react-recipie

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+import Axios from "axios";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogActions from "@material-ui/core/DialogActions";
+import {
+  Header,
+  AppNameComponent,
+  AppIconComponent,
+  SearchComponent,
+  SearchIcon,
+  SearchInput,
+} from "./components/headerComponents";
+
+import {
+  RecipeListContainer,
+  RecipeContainer,
+  CoverImage,
+  IngredientsText,
+  SeeMoreText,
+  RecipeName,
+} from "./components/recipeComponent";
+
+const APP_ID = "cacaa3ce";
+const APP_KEY = "5883d77864536dff0e5ce70ea9b69671	";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Placeholder = styled.img`
+  width: 120px;
+  height: 120px;
+  margin: 200px;
+  opacity: 50%;
+`;
+
+// debouncing
+
+const RecipeComponent = (props) => {
+  const [show, setShow] = useState(false);
+  const { recipeObj } = props;
+  return (
+    <>
+      <Dialog open={show}>
+        <DialogTitle id="alert-dialog-slide-title">Ingredients</DialogTitle>
+
+        <DialogContent>
+          <table>
+            <thead>
+              <th>Ingredients</th>
+              <th>Weight</th>
+            </thead>
+            <tbody>
+              {recipeObj.ingredients.map((ingredientObj) => (
+                <tr>
+                  <td>{ingredientObj.text}</td>
+                  <td>{ingredientObj.weight}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </DialogContent>
+        <DialogActions>
+          <IngredientsText onClick={() => window.open(recipeObj.url)}>
+            See More
+          </IngredientsText>
+          <SeeMoreText onClick={() => setShow(false)}>Close</SeeMoreText>
+        </DialogActions>
+      </Dialog>
+      <RecipeContainer>
+        <CoverImage src={recipeObj.image} />
+        <RecipeName>{recipeObj.label}</RecipeName>
+        <IngredientsText onClick={() => setShow(true)}>
+          Ingredients
+        </IngredientsText>
+        <SeeMoreText onClick={() => window.open(recipeObj.url)}>
+          See More Text
+        </SeeMoreText>
+      </RecipeContainer>
+    </>
+  );
+};
 
 function App() {
+  const [timeoutId, updateTimeoutId] = useState();
+  const [recipeList, updateRecipeList] = useState([]);
+
+  const fetchRecipe = async (searchItem) => {
+    const response = await Axios.get(
+      `https://api.edamam.com/search?q=${searchItem}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    updateRecipeList(response.data.hits);
+    console.log(response);
+  };
+
+  const onTextChange = (event) => {
+    clearTimeout(timeoutId);
+    const timeout = setTimeout(() => fetchRecipe(event.target.value), 500);
+    updateTimeoutId(timeout);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>
+        <AppNameComponent>
+          <AppIconComponent src="healthy-food-logo.svg" />
+          Recipe Finder
+        </AppNameComponent>
+        <SearchComponent>
+          <SearchIcon src="Search_Icon.svg" />
+          <SearchInput placeholder="search" onChange={onTextChange} />
+        </SearchComponent>
+      </Header>
+      <RecipeListContainer>
+        {recipeList.length ? (
+          recipeList.map((recipeObj) => (
+            <RecipeComponent recipeObj={recipeObj.recipe} />
+          ))
+        ) : (
+          <Placeholder src="healthy-food-logo.svg"></Placeholder>
+        )}
+      </RecipeListContainer>
+    </Container>
   );
 }
-
 export default App;
